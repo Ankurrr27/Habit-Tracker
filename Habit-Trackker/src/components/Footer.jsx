78cebfd170dcee38d2650ded29b0c4e19ed4f57f
Ankur <ankurp22singh@gmail.com>
2026-01-02 12:30:32 +0530

    added views

--- Habit-Trackker/src/components/Footer.jsx
+++ Habit-Trackker/src/components/Footer.jsx
@@ -6,18 +6,11 @@ export default function Footer() {
   const [views, setViews] = useState(null);
 
   useEffect(() => {
-    const fetchViews = async () => {
-      try {
-        const res = await api.get("/views");
-        setViews(res.data.count);
-      } catch (err) {
-        // silently fail, footer shouldn't break app
-        console.error("Failed to fetch views");
-      }
-    };
-
-    fetchViews();
-  }, []);
+  fetch("https://habit-tracker-ixsb.onrender.com/views")
+    .then((res) => res.json())
+    .then((data) => setViews(data.count))
+    .catch(() => {});
+}, []);
 
 
   return (
